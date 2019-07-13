2640903cf8350b70b7b2f644844be98d0c652d0a
sj-apiplatform <sj@apiplatform.io>
2019-07-13 19:02:30 +0530

    Adding New Json Editor.

---
+++ ang-jsoneditor/src/jsoneditor/jsoneditor2.component.ts
@@ -0,0 +1,269 @@
+import {
+  Component, ElementRef, Input, OnInit, ViewChild,
+  Output, EventEmitter, forwardRef, ChangeDetectionStrategy
+} from '@angular/core';
+import * as editor from 'jsoneditor';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'json-editor',
+  template: '<div [id]="id" #jsonEditorContainer></div>',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => JsonEditor2Component),
+      multi: true
+    }
+  ],
+  preserveWhitespaces: false,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class JsonEditor2Component implements ControlValueAccessor, OnInit {
+  private editor: any;
+  public id = 'angjsoneditor' + Math.floor(Math.random() * 1000000);
+  disabled = false;
+  isFocused = false;
+
+  public optionsChanged = false;
+
+  @ViewChild('jsonEditorContainer') jsonEditorContainer: ElementRef;
+
+  private _data: Object = {};
+
+  @Input() options: JsonEditorOptions = new JsonEditorOptions();
+  @Input('data')
+  set data(value: Object) {
+    this._data = value;
+    if (this.editor) {
+      this.editor.destroy();
+      this.ngOnInit();
+    }
+  }
+
+  @Output()
+  change: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() { }
+
+
+  ngOnInit() {
+    let optionsBefore = this.options;
+    if (!this.optionsChanged && this.editor) {
+      optionsBefore = this.editor.options;
+    }
+
+    if (!this.options.onChange && this.change) {
+      this.options.onChange = this.onChange.bind(this);
+    }
+
+    const optionsCopy = Object.assign({}, optionsBefore);
+
+    // expandAll is an option only supported by ang-jsoneditor and not by the the original jsoneditor.
+    delete optionsCopy.expandAll;
+
+    this.editor = new editor(this.jsonEditorContainer.nativeElement, optionsCopy, this._data);
+
+    if (this.options.expandAll) {
+      this.editor.expandAll();
+    }
+  }
+
+
+  /**
+   * ngModel
+   * ControlValueAccessor
+   */
+
+  // ControlValueAccessor implementation
+  writeValue(value: any) {
+    this.data = value;
+  }
+
+  // Implemented as part of ControlValueAccessor
+  registerOnChange(fn) {
+    this.onChangeModel = fn;
+  }
+
+  // Implemented as part of ControlValueAccessor.
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
+
+  // Implemented as part of ControlValueAccessor.
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  // Implemented as part of ControlValueAccessor.
+  private onTouched = () => { };
+
+  // Implemented as part of ControlValueAccessor.
+  private onChangeModel = (e) => { };
+
+  public onChange(e) {
+    this.onChangeModel(this.editor.get());
+    this.change.emit(this.editor.get());
+  }
+
+
+  /**
+   * JSON EDITOR FUNCTIONS
+   */
+
+  public collapseAll() {
+    this.editor.collapseAll();
+  }
+
+  public expandAll() {
+    this.editor.expandAll();
+  }
+
+  public focus() {
+    this.editor.focus();
+  }
+
+  public get(): JSON {
+    return this.editor.get();
+  }
+
+  public getMode(): JsonEditorMode {
+    return this.editor.getMode() as JsonEditorMode;
+  }
+
+  public getName(): string {
+    return this.editor.getName();
+  }
+
+  public getText(): string {
+    return this.editor.getText();
+  }
+
+  public set(json: JSON) {
+    this.editor.set(json);
+  }
+
+  public setMode(mode: JsonEditorMode) {
+    this.editor.setMode(mode);
+  }
+
+  public setName(name: string) {
+    this.editor.setName(name);
+  }
+
+  public setSelection(start, end) {
+    this.editor.setSelection(start, end);
+  }
+
+  public getSelection(): any {
+    return this.editor.getSelection();
+  }
+
+  public getValidateSchema(): any {
+    return this.editor.validateSchema;
+  }
+
+  public setSchema(schema: any) {
+    this.editor.setSchema(schema);
+  }
+
+  public search(query: string) {
+    this.editor.search(query);
+  }
+
+  public setOptions(newOptions: JsonEditorOptions) {
+    if (this.editor) {
+      this.editor.destroy();
+    }
+    this.optionsChanged = true;
+    this.options = newOptions;
+    this.ngOnInit();
+  }
+
+  public update(json: JSON) {
+    this.editor.update(json);
+  }
+
+  public destroy() {
+    this.editor.destroy();
+  }
+
+  public isValidJson() {
+    try {
+      JSON.parse(this.getText());
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
+
+export type JsonEditorMode = 'tree' | 'view' | 'form' | 'code' | 'text';
+
+export interface JsonEditorTreeNode {
+  field: String,
+  value: String,
+  path: String[]
+}
+
+export interface IError {
+  path: (string | number)[];
+  message: string;
+}
+
+export class JsonEditorOptions {
+  public ace: any;
+  public ajv: Object;
+  public onChange: () => void;
+  public onEditable: (node: JsonEditorTreeNode | {}) => boolean | { field: boolean, value: boolean };
+  public onError: (error: any) => void;
+  public onModeChange: (newMode: JsonEditorMode, oldMode: JsonEditorMode) => void;
+  public onValidate: (json: Object) => IError[];
+  public escapeUnicode: boolean;
+  public expandAll: boolean;
+  public sortObjectKeys: boolean;
+  public history: boolean;
+  public mode: JsonEditorMode;
+  public modes: JsonEditorMode[];
+  public name: String;
+  public schema: Object;
+  public search: boolean;
+  public indentation: Number;
+  public theme: Number;
+  public language: String;
+  public languages: Object;
+
+  /**
+   * Adds main menu bar - Contains format, sort, transform, search etc. functionality. True
+   * by default. Applicable in all types of mode.
+   */
+  public mainMenuBar: boolean;
+
+  /**
+   * Adds navigation bar to the menu - the navigation bar visualize the current position on
+   * the tree structure as well as allows breadcrumbs navigation.
+   * True by default.
+   * Only applicable when mode is 'tree', 'form' or 'view'.
+   */
+  public navigationBar: boolean;
+
+  /**
+   * Adds status bar to the bottom of the editor - the status bar shows the cursor position
+   * and a count of the selected characters.
+   * True by default.
+   * Only applicable when mode is 'code' or 'text'.
+   */
+  public statusBar: boolean;
+
+  constructor() {
+    this.escapeUnicode = false;
+    this.expandAll = false;
+    this.sortObjectKeys = false;
+    this.history = true;
+    this.mode = 'tree';
+    this.search = true;
+    this.indentation = 2;
+  }
+
+}
--- ang-jsoneditor/src/lib.module.ts
+++ ang-jsoneditor/src/lib.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ModuleWithProviders, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JsonEditorComponent } from './jsoneditor/jsoneditor.component';
+import { JsonEditor2Component } from './jsoneditor/jsoneditor2.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -9,10 +10,12 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   declarations: [
-    JsonEditorComponent
+    JsonEditorComponent,
+    JsonEditor2Component
   ],
   exports: [
-    JsonEditorComponent
+    JsonEditorComponent,
+    JsonEditor2Component
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
